94833fa0ca1700d2489806f22dbf8458ecae97cb
somnath <somnath2004dhere@gmail.com>
2024-10-12 00:33:09 +0530

    this is first commit project in intellij

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,8 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+
 import java.sql.SQLException;
-import java.sql.Statement;
+
 import java.util.List;
 import java.util.Scanner;
 
